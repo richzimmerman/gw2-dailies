2d842552226196d5b7818d79fe6b9c4e42bcf182
rieldfc <rich.zimmerman@gmail.com>
2017-01-02 02:22:38 -0500

    added tomorrow's achievements

--- src/app.js
+++ src/app.js
@@ -2,14 +2,6 @@
 
 // https://api.guildwars2.com/v2/achievements?ids=
 
-
-
-var tomorrowPVE = [];
-var tomorrowPVP = [];
-var tomorrowWVW = [];
-var tomorrowSpec = [];
-var tomorrowRewards = [];
-
 // Gets today's daily achievement ID numbers, pushes them to their respective arrays,
 // then gets the details and builds the html - on pageload.
 (function getTodaysData() {
@@ -22,7 +14,6 @@ var tomorrowRewards = [];
 		let todayPVP = [];
 		let todayWVW = [];
 		let todaySpec = [];
-		console.log(data);
 		for (let i = 0; i < data.pve.length; i++) {
 			todayPVE.push(data.pve[i].id);
 		}
@@ -54,7 +45,6 @@ function getAchievements(postList, url, cat) {
 		dataType: "json"
 	}).then(function (data){
 		var todayRewards = [];
-		console.log(data);
 		for (let i = 0; i < data.length; i++) {
 			todayRewards.push(data[i].rewards[0].id);
 			let tr = document.createElement("tr");
@@ -117,6 +107,99 @@ function getTodaysAchievementsURL(array) {
 	}
 	return achieveUrl;
 }
+/******* Tomorrow *****************************************************/
+(function getTomorrowsAchievements(){
+	$.ajax({
+		method: "GET",
+		url: "https://api.guildwars2.com/v2/achievements/daily/tomorrow",
+		dataType: "json"
+	}).then(function (data){
+		var tomorrowPVE = [];
+		var tomorrowPVP = [];
+		var tomorrowWVW = [];
+		var tomorrowSpec = [];
+		console.log(data);
+		for (let i = 0; i < data.pve.length; i++) {
+			tomorrowPVE.push(data.pve[i].id);
+		}
+		for (let i = 0; i < data.pvp.length; i++) {
+			tomorrowPVP.push(data.pvp[i].id);
+		}
+		for (let i = 0; i < data.wvw.length; i++) {
+			tomorrowWVW.push(data.wvw[i].id);
+		}
+		if (data.special) {
+			for (let i = 0; i < data.special.length; i++) {
+				tomorrowSpec.push(data.special[i].id);
+			}
+		}
+		getTomAchievements(tomPostList("pve"), getTodaysAchievementsURL(tomorrowPVE), "pve")
+		getTomAchievements(tomPostList("pvp"), getTodaysAchievementsURL(tomorrowPVP), "pvp");
+		getTomAchievements(tomPostList("wvw"), getTodaysAchievementsURL(tomorrowWVW), "wvw");
+		if (tomorrowSpec) {
+			getTomAchievements(tomPostList("special"), getTodaysAchievementsURL(tomorrowSpec), "special");
+		}	
+	});
+})();
+
+// Gets the achievement details and builds the HTML
+function getTomAchievements(postList, url, cat) {
+	return $.ajax({
+		method: "GET",
+		url: url,
+		dataType: "json"
+	}).then(function (data){
+		var tomorrowRewards = [];
+		console.log(data);
+		for (let i = 0; i < data.length; i++) {
+			tomorrowRewards.push(data[i].rewards[0].id);
+			let tr = document.createElement("tr");
+			tr.className = "col-xs-12"
+			tr.innerHTML += "<td class='col-xs-3'>"+data[i].name+":</td>";
+			tr.innerHTML += "<td class='col-xs-8 col-xs-offset-1'>"+data[i].requirement+"</td>";
+			if (cat == "pve") {
+				$("#tom-pve").append(tr);
+			} else if (cat == "pvp") {
+				$("#tom-pvp").append(tr);
+			} else if (cat == "wvw") {
+				$("#tom-wvw").append(tr);
+			} else if (cat == "special") {
+				$("#tom-special").append(tr);
+			} else {
+				return;
+			}
+			
+		}
+		let img = document.createElement("img");
+		// img.className = "fluid-image";
+		img.src = data[0].icon;
+		if (cat == "pvp") {
+			$("#tom-pvp").prepend(img);
+		} else if (cat == "wvw") {
+			$("#tom-wvw").prepend(img);
+		} else if (cat == "special") {
+			$("#tom-special").prepend(img);
+		} else {
+			return;
+		}
+	});
+}
+
+function tomPostList(cat) {
+	if (cat == "pve") {
+		$("#tom-pve").append("<h3>PvE:</h3>");
+	} else if (cat == "pvp") {
+		$("#tom-pvp").append("<h3>PvP:</h3>");
+	} else if (cat == "wvw") {
+		$("#tom-wvw").append("<h3>WvW:</h3>");
+	} else if (cat == "special") {
+		$("#tom-special").append("<h3>Special:</h3>");
+	} else {
+		return;
+	}
+}
+
+
 
 
 //  Figure these rewards functions out later. (multiple global arrays?)
@@ -143,6 +226,23 @@ function getRewards(url) {
 	});
 }
 
+$("#today-button").on("click", function(){
+	$("#navbar2").hide(0, function (){
+		$("#navbar1").show(0);
+	});
+	$("#tomorrow").fadeOut(500, function(){
+		$("#today").fadeIn(500);
+	});
+});
+
+$("#tomorrow-button").on("click", function(){
+	$("#navbar1").hide(0, function (){
+		$("#navbar2").show(0);
+	});
+	$("#today").fadeOut(500, function(){
+		$("#tomorrow").fadeIn(500);
+	});
+});
 
 
 // Navbar scroll animation
