6af2d838662f3194b0c2cd547ea895dc3ef0d04b
rieldfc <rich.zimmerman@gmail.com>
2017-01-01 23:13:47 -0500

    Added styling and finished Today's achievements sans rewards

--- src/app.js
+++ src/app.js
@@ -2,23 +2,26 @@
 
 // https://api.guildwars2.com/v2/achievements?ids=
 
-var todayPVE = [];
-var todayPVP = [];
-var todayWVW = [];
-var todaySpec = [];
-var todayRewards = [];
+
+
 var tomorrowPVE = [];
 var tomorrowPVP = [];
 var tomorrowWVW = [];
 var tomorrowSpec = [];
 var tomorrowRewards = [];
 
+// Gets today's daily achievement ID numbers, pushes them to their respective arrays,
+// then gets the details and builds the html - on pageload.
 (function getTodaysData() {
 	$.ajax({
 		method: "GET",
 		url: "https://api.guildwars2.com/v2/achievements/daily",
 		dataType: "json"
 	}).then(function (data){
+		let todayPVE = [];
+		let todayPVP = [];
+		let todayWVW = [];
+		let todaySpec = [];
 		console.log(data);
 		for (let i = 0; i < data.pve.length; i++) {
 			todayPVE.push(data.pve[i].id);
@@ -29,10 +32,11 @@ var tomorrowRewards = [];
 		for (let i = 0; i < data.wvw.length; i++) {
 			todayWVW.push(data.wvw[i].id);
 		}
-		for (let i = 0; i < data.special.length; i++) {
-			todaySpec.push(data.special[i].id);
+		if (data.special) {
+			for (let i = 0; i < data.special.length; i++) {
+				todaySpec.push(data.special[i].id);
+			}
 		}
-
 		getAchievements(postList("pve"), getTodaysAchievementsURL(todayPVE), "pve")
 		getAchievements(postList("pvp"), getTodaysAchievementsURL(todayPVP), "pvp");
 		getAchievements(postList("wvw"), getTodaysAchievementsURL(todayWVW), "wvw");
@@ -42,31 +46,51 @@ var tomorrowRewards = [];
 	});
 })();
 
+// Gets the achievement details and builds the HTML
 function getAchievements(postList, url, cat) {
 	return $.ajax({
 		method: "GET",
 		url: url,
 		dataType: "json"
 	}).then(function (data){
-		console.log(data, cat);
+		var todayRewards = [];
+		console.log(data);
 		for (let i = 0; i < data.length; i++) {
-			var html = "<br /><td class='col-md-3'>"+ data[i].name + ":</td>" +
-				"<td class='col-md-8 col-md-offset-1'> " + data[i].requirement + "</td>";
+			todayRewards.push(data[i].rewards[0].id);
+			let tr = document.createElement("tr");
+			tr.className = "col-xs-12"
+			tr.innerHTML += "<td class='col-xs-3'>"+data[i].name+":</td>";
+			tr.innerHTML += "<td class='col-xs-8 col-xs-offset-1'>"+data[i].requirement+"</td>";
 			if (cat == "pve") {
-				$("#pve").append(html);
+				$("#pve").append(tr);
 			} else if (cat == "pvp") {
-				$("#pvp").append(html);
+				$("#pvp").append(tr);
 			} else if (cat == "wvw") {
-				$("#wvw").append(html);
+				$("#wvw").append(tr);
 			} else if (cat == "special") {
-				$("#special").append(html);
+				$("#special").append(tr);
 			} else {
 				return;
 			}
+			
+		}
+		let img = document.createElement("img");
+		// img.className = "fluid-image";
+		img.src = data[0].icon;
+		if (cat == "pvp") {
+			$("#pvp").prepend(img);
+		} else if (cat == "wvw") {
+			$("#wvw").prepend(img);
+		} else if (cat == "special") {
+			$("#special").prepend(img);
+		} else {
+			return;
 		}
 	});
 }
 
+// Adds headline element for each category (This is necessary since the Special category
+// is not a year-round achievement category)
 function postList(cat) {
 	if (cat == "pve") {
 		$("#pve").append("<h3>PvE:</h3>");
@@ -81,6 +105,7 @@ function postList(cat) {
 	}
 }
 
+// Builds the URL with achievement IDs for next GET request
 function getTodaysAchievementsURL(array) {
 	achieveUrl = "https://api.guildwars2.com/v2/achievements?ids="
 	for (let i = 0; i < array.length; i++) {
@@ -90,6 +115,41 @@ function getTodaysAchievementsURL(array) {
 			achieveUrl += array[i]+",";
 		}
 	}
-	console.log(achieveUrl);
 	return achieveUrl;
 }
+
+
+//  Figure these rewards functions out later. (multiple global arrays?)
+function getRewardsURL(array) {
+	rewardUrl = "https://api.guildwars2.com/v2/items?ids=";
+	for (let i = 0; i < array.length; i++) {
+		if (i == array.length - 1) {
+			rewardUrl += array[i];
+		} else {
+			rewardUrl += array[i]+",";
+		}
+	}
+	console.log(rewardUrl);
+	return rewardUrl;
+}
+
+function getRewards(url) {
+	return $.ajax({
+		method: "GET",
+		url: url,
+		dataType: "json"
+	}).then(function(data){
+		console.log(data);
+	});
+}
+
+
+
+// Navbar scroll animation
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 500);
+    $(this).blur();
+    return false;
+});
